cbca32257389aa828a5a5e654c27611a5b57a8ae
user <aishh828@gmail.com>
2024-03-21 13:39:22 +0530

    new commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import "../styles/home.css";
 const Home = () => {
-    return (
-      <div>
-      <h1 id='hheading'>Home Page</h1>
-      <Link to ="/firstaid">
-        <button className='hbutton'>First Aid</button>
-      </Link>
-      <Link to ="/emergency">
-        <button className='hbutton'>Emergency Medical Care</button>
-      </Link>
-      <Link to ="/police">
-        <button className='hbutton'>Police</button>
-      </Link>
-      <Link to ="/fireforce">
-        <button className='hbutton'>Fire Force</button>
-      </Link>
 
-      </div>
-    );
-  }
+  const [categories, setCategoryList] = useState([])
+  useEffect(() => {
+    // API Call via bnodejs to DB to fetch the list of categories
+    // eslint-disable-next-line
+    const a = fetch("http://localhost:5000/categories").then(res => res.json()).then(
+      data => {
+        setCategoryList(data);
+      }
+    )
+  }, [])
+  //console.log(categories)
+  return (
+    <div>
+      <h1 id='hheading'>Home Page</h1>
+        {categories.map((category) => (
+          <Link to ={category.category_url}>
+            <button className='hbutton'>{category.categories_name}</button>
+          </Link>
+          ))}
+    </div>
+  );
+}
 export default Home;
