1263071f265d42b8a9596c07c70404b8f78aa60d
AnaghaVenu23 <anaghavenu2003@gmail.com>
2024-03-28 23:21:58 +0530

    Second Commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import "../styles/home.css";
 import {Link} from "react-router-dom";
-// import Img1 from "../assets/firstaid.jpg";
-// import Img2 from "../assets/medicalcare.avif";
-// import Img3 from "../assets/police.jpg";
-// import Img4 from "../assets/firestation.avif";
-const Home = () => {
-  const [categories, setCategoryList] = useState([])
-  useEffect(() => {
-    // API Call via bnodejs to DB to fetch the list of categories
-    // eslint-disable-next-line
-    const a = fetch("http://localhost:5000/categories").then(res => res.json()).then(
-      data => {
-        setCategoryList(data);
-      }
-    )
-  }, [])
-  //console.log(categories)
-  return (
-    <div>
-      <h1 id='hheading'>Home Page</h1>
-        {categories.map((category) => (
-          <Link to ={category.category_url}>
-            <button className='hbutton'>{category.categories_name}</button>
-          </Link>
-          ))}
-    </div>
-  );
-}
+import Img1 from "../assets/firstaid.jpg";
+import Img2 from "../assets/medicalcare.avif";
+import Img3 from "../assets/police.jpg";
+import Img4 from "../assets/firestation.avif";
+  const Home = () => {
+    return (
+      <div>
+        <div className='top'>
+        <Link to ="/firstaid">
+          <button className='hbutton' id='btn1'><img  id='img1' src={Img1} alt='img not found'/> First Aid</button>
+        </Link>
+        <Link to ="/emergency">
+          <button className='hbutton' id='btn2'><img  id='img2' src={Img2} alt='img not found'/>Emergency <br/>Medical Care</button>
+        </Link>
+        </div>
+
+      <div className='bottom'>
+        <Link to ="/police">
+          <button className='hbutton' id='btn3'><img  id='img3' src={Img3} alt='img not found'/>Police</button>
+        </Link>
+        <Link to ="/fireforce">
+          <button className='hbutton' id='btn4'><img  id='img4' src={Img4} alt='img not found'/>Fire Force</button>
+        </Link>
+      </div>
+  
+      </div>
+    );
+  }
 export default Home;
